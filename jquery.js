3d8175042fa6111d0f117331bd5c8aee71d8dec6
Silviu Stoian <silviu.stoian96@gmail.com>
2016-07-29 19:48:27 +0300

    update README

--- jquery.js
+++ jquery.js
@@ -1,15 +1,36 @@
-
+var size=16;
+var squareSize=960/size;
 
 $(document).ready(function(){
-     for (var i = 0; i < 16; i++) {
+
+
+
+
+
+     for (var i = 0; i < size; i++) {
        $('.wrapper').append("<div class='square'></div>");
+       $(".square").css({
+         "display": "inline-block",
+      "width": squareSize + "px",
+      "height": squareSize + "px",
+
+      "background-color": "#999999",
+
+       });
      }
 
-     for (var i = 0; i < 16; i++) {
+
+     for (var i = 0; i < size; i++) {
        $('.square').append("<div class='squarec'></div>");
+       $(".squarec").css({
+         "display": "inline-block",
+      "width": squareSize + "px",
+      "height": squareSize + "px",
+      "outline": "1px solid",
+      "background-color": "#999999",
+
+       });
      }
-
-
 
 
      $('.squarec').hover(function(){
@@ -18,11 +39,53 @@ $(document).ready(function(){
      });
 
     $('#butt').click(function(){
-        $('.squarec').css('background-color','#999999');
-        var what=prompt("ENTER new size:");
+
+        killGrid();
+        var size=prompt("ENTER new size:");
+        var squareSize=960/size;
+
+        for (var i = 0; i < size; i++) {
+          $('.wrapper').append("<div class='square'></div>");
+          $(".square").css({
+            "display": "inline-block",
+         "width": squareSize + "px",
+         "height": squareSize + "px",
+
+         "background-color": "#999999",
+
+          });
+        }
+
+
+        for (var i = 0; i < size; i++) {
+          $('.square').append("<div class='squarec'></div>");
+          $(".squarec").css({
+            "display": "inline-block",
+         "width": squareSize + "px",
+         "height": squareSize + "px",
+         "outline": "1px solid",
+         "background-color": "#999999",
+
+          });
+        }
+
+        $('.squarec').hover(function(){
+            $(this).css('background-color','black');
+
+        });
 
     });
 
+    function killGrid() {
+     $('.wrapper').children().remove();
+   }
 
+    $('#butt2').click(function(){
+      $('.squarec').hover(function(){
+    var randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+    $(this).css('background-color',randomColor);
 
+  });
+
+  });
 });
